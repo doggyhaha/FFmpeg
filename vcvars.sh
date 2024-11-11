143a7abb07bef442d532331737b24cdcfb73d33b
doggyhaha <75123663+doggyhaha@users.noreply.github.com>
2024-11-11 22:55:27 +0100

    GEOLIER

--- vcvars.sh
+++ vcvars.sh
@@ -5,23 +5,37 @@ windows_kits_base_path="/c/Program Files (x86)/Windows Kits/10/Include"
 get_vs_edition() {
     #get year and edition
     local vs_year=$(ls -1 "$vs_base_path" | grep -Eo '[0-9]{4}')
+    #get latest year
+    vs_year=$(echo "$vs_year" | sort -nr | head -n1)
     #new base path
     local vs_base_path="$vs_base_path/$vs_year"
     #get edition
     local vs_edition=$(ls -1 "$vs_base_path" | grep -Eo 'Community|Professional|Enterprise')
+    #get the best edition in order enterprise, professional, community
+    if [ -d "$vs_base_path/Enterprise" ]; then
+        vs_edition="Enterprise"
+    elif [ -d "$vs_base_path/Professional" ]; then
+        vs_edition="Professional"
+    elif [ -d "$vs_base_path/Community" ]; then
+        vs_edition="Community"
+    fi
     echo "$vs_year/$vs_edition"
 }
 
 get_msvc_version() {
     #get msvc version
-    vs_edition = $1
+    vs_edition=$1
     local msvc_version=$(ls -1 "$vs_base_path/$vs_edition/VC/Tools/MSVC" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+    #get latest version
+    msvc_version=$(echo "$msvc_version" | sort -nr | head -n1)
     echo "$msvc_version"
 }
 
 get_windows_kits_version() {
     #get windows kits version
     local windows_kits_version=$(ls -1 "$windows_kits_base_path" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+    #get latest version
+    windows_kits_version=$(echo "$windows_kits_version" | sort -nr | head -n1)
     echo "$windows_kits_version"
 }
 
